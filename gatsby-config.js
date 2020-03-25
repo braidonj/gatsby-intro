801cc552d6a6299b538d1e216099d7ca161f196b
braidonj <47284747+braidonj@users.noreply.github.com>
2020-03-25 15:31:29 -0600

    Finished GraphQL intro section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description: 'A site we built during a workshop.',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
